1af9bc0f1e013fd426a4bf2e7e9ecefb31fda0ab
pozabil <partnet.biz@gmail.com>
2021-09-08 21:35:43 +0300

    little Test#filter_by_category fix (v.0.3.1)

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,7 +4,7 @@ class Test < ApplicationRecord
   has_and_belongs_to_many :users
 
   def self.filter_by_category(category)
-    includes(:category).where(categories: { title: category })
+    joins(:category).where(categories: { title: category })
       .order(title: :desc).distinct.pluck(:title)
   end
 end
