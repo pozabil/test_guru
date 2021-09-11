e96ec485462802616476212cb491477ef2407ac1
pozabil <partnet.biz@gmail.com>
2021-09-12 02:01:39 +0300

    fix answer validation (v.0.5.1)

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,7 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
+  validates_associated :question
 
   scope :only_correct, -> { where(correct: true) }
 end
