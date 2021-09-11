6e45255bd881fa93ec3e8e4e5978dd38518db9b7
pozabil <partnet.biz@gmail.com>
2021-09-12 01:42:29 +0300

    scopes and validates added (v.0.5.0)

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,13 @@
 class Question < ApplicationRecord
   belongs_to :test
   has_many :answers
+
+  validates :body, presence: true
+  validate :validate_number_of_answers
+
+  private
+
+  def validate_number_of_answers
+    errors.add(:base, 'wrong number of answers') unless answers.size.between?(1, 4)
+  end
 end
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,3 +1,3 @@
 class TestGuru::Application
-  VERSION = "0.4.1"
+  VERSION = "0.5.0"
 end
