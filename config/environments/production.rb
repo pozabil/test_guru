22a6582f909dd190aab0b72754fa6b35d26121b0
pozabil <partnet.biz@gmail.com>
2021-12-05 23:20:30 +0300

    smtp added (v.0.16.1)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options= { host: 'test-guru-by-chislo-pi.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smpp_settings = {
+  config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     user_name:            ENV['SMTP_USERNAME'],
